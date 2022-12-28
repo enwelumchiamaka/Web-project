d51f7358ec9d5e15cc553ce122bb4a7240630740
Amaka <enwelumchiamaka@gmail.com>
2022-12-28 15:35:23 +0100

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>blog</title>
+<link rel="stylesheet" type="text/css" href="assest/css/blog.css" />
+</head>
+
+<body>
+<div class="container-fluid">
+<h1 style="background-color: #FF0;text-align:center;padding:15px;color:#000">My Blog About My Life</h1>
+
+<div class="row">
+
+<!---menus--->
+<div class="column side Design3" id="column1">
+  <h3>Menu</h3>
+  <p class="me"><a href="#">Today</a></p>
+  <p class="me"><a href="#">Yesterday</a></p>
+  <p class="me"><a href="#">Last Week</a></p>
+  <p class="me"><a href="#">Archive</a></p>
+</div>
+<!---content---->
+<div class="column middle Design2">
+<h2>Meeting with my Supervisor</h2>
+<p>14 Dec 2011</p>
+<h4>Today I went to the university by bus</h4>
+<h4>I had a met with my PHD supervisor</h4>
+<br />
+<h2>New Car!!!</h2>
+<p>12 Dec 2011</p>
+<h4>Today I bought my new car.It's a Honda Accord and it's really nice.</h4>
+<h4>I met some friends at a pub.</h4>
+<br />
+<h2>Visit My Parents</h2>
+<p>10 Dec 2011</p>
+<h4>Triend to contact my PHD supervisor. He wasout of his office</h4>
+<h4>I visited my parents and we had a nice dinner together.</h4>
+</div>
+<div class="column side Design" id="column">
+<ul class="ul">
+<h2>Last Posts</h2>
+<li><a href="#">Meeting With Supervisor</a></li>
+<li><a href="#">New Car!!!</a></li>
+<li><a href="#">Visit My Parent</a></li>
+</ul>
+</div>
+</div>
+<br />
+<h4 align="center">Contact Us<a href="#"> email@something.com</a></h4>
+
+</div>
+</body>
+</html>
